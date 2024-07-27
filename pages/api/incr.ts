200a073bffca9858ef4c84b52996aea49ef5b596
Amjad Al-Rahal <40330941+amjad-developer@users.noreply.github.com>
2024-07-27 11:56:03 +0300

    process env

--- pages/api/incr.ts
+++ pages/api/incr.ts
@@ -1,8 +1,11 @@
 import { Redis } from "@upstash/redis";
 import { NextRequest, NextResponse } from "next/server";
 
-const redis = Redis.fromEnv();
-export const config = {
+export const db: Redis = new Redis({
+	url: process.env.UPSTASH_REDIS_REST_URL || '',
+	token: process.env.UPSTASH_REDIS_REST_TOKEN || ''
+ });
+ export const config = {
 	runtime: "edge",
 };
 
