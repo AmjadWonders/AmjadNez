82ba0e62db32412398839ef1b39f58bde4a9e45c
Amjad Al-Rahal <40330941+AmjadWonders@users.noreply.github.com>
2024-11-19 15:59:12 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -58,14 +58,14 @@ export default function Home() {
 					<h2 className="text-sm text-zinc-500 duration-500 hover:text-zinc-300">
 					Driven to deliver exceptional results in fast-paced environments
 					</h2>
-					<a onClick={toggleModal} className="text-sm text-indigo-700 duration-150 hover:text-indigo-400 font-medium inline-flex items-center gap-x-1 cursor-pointer">
+					<!-- <a onClick={toggleModal} className="text-sm text-indigo-700 duration-150 hover:text-indigo-400 font-medium inline-flex items-center gap-x-1 cursor-pointer">
 						Updated CV
 						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
 							<path fillRule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clipRule="evenodd" />
 						</svg>
-					</a>
+					</a> -->
 				</div>
-				<PdfModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
+				<!-- <PdfModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} /> -->
 			</div>
 		</div>
 	);
